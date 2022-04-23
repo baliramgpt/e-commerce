64ebe0b58cfb36ab5f3601518f43b17796db29cf
Baliram Kumar Gupta <baliramgpt@gmail.com>
2022-04-23 14:58:40 +0530

    redux-thunk and spinner implementation
    redux-thunk and spinner implementation

--- src/store/store.js
+++ src/store/store.js
@@ -4,20 +4,26 @@ import {compose, createStore, applyMiddleware} from 'redux';
 import { persistStore,persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
-
 
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist : ['user'],
+    whitelist : ['cart'],
 }
 
 const persistedReducer = persistReducer(persistConfig,rootReducer);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk,].filter(Boolean);
+
+// const thunkMiddleware = (store) => (next) => (action) =>{
+//     if(typeof(action) === 'function'){
+//         action(dispatch);
+//     }
+// }
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ) || compose;
 
